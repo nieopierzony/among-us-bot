756e43f524377a7c50748db2c5d1aba099488d8d
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2020-11-17 01:19:40 +0100

    added returnmessage helper

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,5 @@
 import { mainKeyboard, backKeyboard, regionsKeyboard } from './keyboards.js';
 import sceneFactory from './sceneFactory.js';
+import returnMessage from './returnMessage.js';
 
-export { mainKeyboard, backKeyboard, regionsKeyboard, sceneFactory };
+export { mainKeyboard, backKeyboard, regionsKeyboard, sceneFactory, returnMessage };
---
+++ src/helpers/returnMessage.js
@@ -0,0 +1,7 @@
+export default (ctx, message, markup = Extra.HTML()) => {
+  if (ctx.updateType === 'callback_query') {
+    ctx.editMessageText(message, markup);
+  } else {
+    ctx.replyWithHTML(message, markup);
+  }
+};
