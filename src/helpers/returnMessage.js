18c7418759c781ad0a5e344780c5039b891e27bf
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2020-11-17 02:11:47 +0100

    fix(returnMessage): import dependencies

--- src/helpers/returnMessage.js
+++ src/helpers/returnMessage.js
@@ -1,3 +1,6 @@
+import Telegraf from 'telegraf';
+const { Extra } = Telegraf;
+
 export default (ctx, message, markup = Extra.HTML()) => {
   if (ctx.updateType === 'callback_query') {
     ctx.editMessageText(message, markup);
