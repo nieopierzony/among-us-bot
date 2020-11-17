12d93cd20fe7ff27ab49aea6541365cbe882460b
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2020-11-17 01:01:41 +0100

    Added regions keyboard

--- src/helpers/keyboards.js
+++ src/helpers/keyboards.js
@@ -1,11 +1,26 @@
 import Telegraf from 'telegraf';
 
-const { Markup } = Telegraf;
+const { Markup, Extra } = Telegraf;
 
-export function getBackKeyboard(ctx) {
+export function backKeyboard(ctx) {
   return Markup.keyboard([ctx.i18n.t('keys.back')])
     .resize()
     .extra();
+}
+
+export function regionsKeyboard(ctx) {
+  return Extra.HTML().markup(m =>
+    m.inlineKeyboard(
+      [
+        [
+          m.callbackButton(ctx.i18n.t('keys.regions.europe'), 'region::eu'),
+          m.callbackButton(ctx.i18n.t('keys.regions.america'), 'region::na'),
+        ],
+        [m.callbackButton(ctx.i18n.t('keys.regions.asia'), 'region::as')],
+      ],
+      {},
+    ),
+  );
 }
 
 export function mainKeyboard(ctx) {
