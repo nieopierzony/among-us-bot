ee1a215740ebf9d7290fbbb3a302724ef5a9a1e2
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2020-11-17 01:02:06 +0100

    All keyboards moved to inline

--- src/helpers/keyboards.js
+++ src/helpers/keyboards.js
@@ -1,30 +1,25 @@
 import Telegraf from 'telegraf';
 
-const { Markup, Extra } = Telegraf;
+const { Extra } = Telegraf;
 
 export function backKeyboard(ctx) {
-  return Markup.keyboard([ctx.i18n.t('keys.back')])
-    .resize()
-    .extra();
+  return Extra.HTML().markup(m => m.inlineKeyboard([m.callbackButton(ctx.i18n.t('keys.back'), 'main')]));
 }
 
 export function regionsKeyboard(ctx) {
   return Extra.HTML().markup(m =>
-    m.inlineKeyboard(
+    m.inlineKeyboard([
       [
-        [
-          m.callbackButton(ctx.i18n.t('keys.regions.europe'), 'region::eu'),
-          m.callbackButton(ctx.i18n.t('keys.regions.america'), 'region::na'),
-        ],
-        [m.callbackButton(ctx.i18n.t('keys.regions.asia'), 'region::as')],
+        m.callbackButton(ctx.i18n.t('keys.regions.europe'), 'region::EU'),
+        m.callbackButton(ctx.i18n.t('keys.regions.america'), 'region::NA'),
       ],
-      {},
-    ),
+      [m.callbackButton(ctx.i18n.t('keys.regions.asia'), 'region::AS')],
+    ]),
   );
 }
 
 export function mainKeyboard(ctx) {
-  return Markup.keyboard([[ctx.i18n.t('keys.newGame')]])
-    .resize()
-    .extra();
+  return Extra.HTML().markup(m =>
+    m.inlineKeyboard([m.callbackButton(ctx.i18n.t('keys.newGame'), 'new_game')]),
+  );
 }
