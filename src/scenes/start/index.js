bba216daa5f736af45b41e5a72f25dafa9228dc3
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2020-11-17 01:04:53 +0100

    set newGame scene only for groups

--- src/scenes/start/index.js
+++ src/scenes/start/index.js
@@ -1,11 +1,13 @@
 import I18n from 'telegraf-i18n';
 import { mainKeyboard, sceneFactory } from '../../helpers/index.js';
+import { onlyGroups } from '../../middlewares/index.js';
 
 const { match } = I18n;
 const scene = sceneFactory('start');
 
-scene.enter(ctx => ctx.replyWithHTML(ctx.i18n.t('start.enter'), mainKeyboard(ctx)));
-scene.hears(match('keys.newGame'), ctx => ctx.scene.enter('newGame'));
-scene.on('message', ({ replyWithHTML, i18n }) => replyWithHTML(i18n.t('menu.wrong')));
+scene.enter(onlyGroups, ctx => ctx.replyWithHTML(ctx.i18n.t('start.enter'), mainKeyboard(ctx)));
+
+scene.hears(match('keys.newGame'), ctx => ctx.scene.enter('checkGameCode'));
+scene.on('message', ({ replyWithHTML, i18n }) => replyWithHTML(i18n.t('errors.menuError')));
 
 export default scene;
