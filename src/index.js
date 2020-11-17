89675f18482f345b59215ae391e162e86d6145e1
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2020-11-17 01:02:50 +0100

    Added session for groups

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import mongoose from 'mongoose';
 import Telegraf from 'telegraf';
 import TelegrafI18n from 'telegraf-i18n';
 
-import { registerUser } from './middlewares/index.js';
-import { startScene } from './scenes/index.js';
+import { onlyGroups, registerUser } from './middlewares/index.js';
+import { startScene, checkGameCode, checkRegion } from './scenes/index.js';
 
 const { BOT_TOKEN, DB_URL } = process.env;
 const bot = new Telegraf(BOT_TOKEN);
@@ -21,14 +21,28 @@ const i18n = new TelegrafI18n({
 });
 
 const { Stage, session } = Telegraf;
-const stage = new Stage([startScene]);
+const stage = new Stage([startScene, checkGameCode, checkRegion]);
 
-bot.use(session());
+bot.use(session({ ttl: 60 * 5 }));
+bot.use(
+  session({
+    property: 'group',
+    getSessionKey: ctx => {
+      if (ctx.from && ctx.chat && ['supergroup', 'group'].includes(ctx.chat.type)) {
+        return `${ctx.chat.id}`;
+      }
+      return null;
+    },
+    ttl: 60 * 5,
+  }),
+);
+
 bot.use(i18n.middleware());
 bot.use(stage.middleware());
 bot.use(registerUser);
 
 bot.start(({ scene }) => scene.enter('start'));
+bot.command(onlyGroups, ({ scene }) => scene.enter('start'));
 bot.on('message', ({ scene }) => scene.enter('start'));
 
 bot.startPolling();
